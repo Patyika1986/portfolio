31b07a67e6d1ecf1f2f4971b78e3a5ea66a4ea15
Peter <peter.lakatos@outlook.de>
2023-08-04 14:37:15 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
+import { AboutMeComponent } from './component/about-me/about-me.component';
+import { ProjectsComponent } from './component/projects/projects.component';
+import { ContactComponent } from './component/contact/contact.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'ueber-mich', component: AboutMeComponent },
+  { path: 'projekte', component: ProjectsComponent },
+  { path: 'kontakt', component: ContactComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
 
